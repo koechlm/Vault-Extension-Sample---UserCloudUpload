5d7f781ade9a215b7c0ca2bb4ebfe02110000422
Markus Koechl <markus.koechl@autodesk.com>
2022-07-04 19:23:43 +0200

    Add project files.

---
+++ Vault-UserCloudUpload/VaultExtension.cs
@@ -0,0 +1,171 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using ACW = Autodesk.Connectivity.WebServices;
+using ACWT = Autodesk.Connectivity.WebServicesTools;
+using VDF = Autodesk.DataManagement.Client.Framework;
+using Autodesk.Connectivity.Explorer.Extensibility;
+using Autodesk.Connectivity.Extensibility.Framework;
+
+[assembly: ApiVersion("16.0")]
+[assembly: ExtensionId("6b8d6d58-b035-49ce-95d7-abdebb0404e1")]
+
+namespace VaultUserCloudUpload
+{
+    public class VaultExtension : IExplorerExtension
+    {
+        public static VDF.Vault.Currency.Connections.Connection mConnection = null;
+        public static Settings mSettings = null;
+        public static bool mConfigPerm = false;
+        public static bool mSettingsChanged = false;
+        public bool mIsDarkTheme = VDF.Forms.SkinUtils.ThemeState.IsDarkTheme;
+
+
+        IEnumerable<CommandSite> IExplorerExtension.CommandSites()
+        {
+            List<CommandSite> mVaultExtensionCmdSites = new List<CommandSite>();
+
+            //Describe admin command item
+            CommandItem mAdminOptionsCmd = new CommandItem("Command.VaultUserUploadAdminForm", "Configure User Cloud Upload...")
+            {
+                Image = Properties.Resources.AdminOptionsImage_16_light
+            };
+            mAdminOptionsCmd.Execute += mAdminCmd_Execute;
+
+            //Deploy command site
+            CommandSite mAdminCmdSite = new CommandSite("Menu.ToolsMenu", "VaultUserUploadAdminOptions")
+            {
+                Location = CommandSiteLocation.ToolsMenu,
+                DeployAsPulldownMenu = false
+            };
+            mAdminCmdSite.AddCommand(mAdminOptionsCmd);
+            mVaultExtensionCmdSites.Add(mAdminCmdSite);
+
+            //describe user context menu commands
+            CommandItem mUploadToCloudProjectCmd = new CommandItem("Command.UploadToCloudProject", "Upload to Cloud Project");
+            mUploadToCloudProjectCmd.NavigationTypes = new SelectionTypeId[] { SelectionTypeId.File };
+            mUploadToCloudProjectCmd.MultiSelectEnabled = true;
+            mUploadToCloudProjectCmd.Hint = "Uploads selected file(s) to the corresponding or configured cloud project";
+            if (Autodesk.DataManagement.Client.Framework.Forms.SkinUtils.WinFormsTheme.Instance.CurrentTheme == VDF.Forms.SkinUtils.Theme.Light)
+            {
+                mUploadToCloudProjectCmd.Image = Properties.Resources.cmdPush_16_light;
+            }
+            else
+            {
+                mUploadToCloudProjectCmd.Image = Properties.Resources.cmdPush_16_dark;
+            }
+
+            CommandItem mUploadToCloudCmd = new CommandItem("Command.UploadToCloud", "Upload to ...");
+            mUploadToCloudCmd.NavigationTypes = new SelectionTypeId[] { SelectionTypeId.File };
+            mUploadToCloudCmd.MultiSelectEnabled = true;
+            mUploadToCloudCmd.Hint = "Select Cloud Drive and upload selected file(s)";
+            if (Autodesk.DataManagement.Client.Framework.Forms.SkinUtils.WinFormsTheme.Instance.CurrentTheme == VDF.Forms.SkinUtils.Theme.Light)
+            {
+                mUploadToCloudCmd.Image = Properties.Resources.GlobalFolderView_16_light;
+            }
+            else
+            {
+                mUploadToCloudCmd.Image = Properties.Resources.GlobalFolderView_16_dark;
+            }
+
+            //deploy command site for the file context commands
+            CommandSite mUploadToCloudCmdSite = new CommandSite("Menu.FileContextMenu", "Upload to Cloud");
+            mUploadToCloudCmdSite.Location = CommandSiteLocation.FileContextMenu;
+            mUploadToCloudCmdSite.DeployAsPulldownMenu = false;
+            mUploadToCloudCmdSite.AddCommand(mUploadToCloudProjectCmd);
+            mUploadToCloudCmdSite.AddCommand(mUploadToCloudCmd);
+
+            mVaultExtensionCmdSites.Add(mUploadToCloudCmdSite);
+
+            return mVaultExtensionCmdSites;
+        }
+
+        private void mAdminCmd_Execute(object sender, CommandItemEventArgs e)
+        {
+            Autodesk.Connectivity.WebServices.Permis[] mAllPermisObjects = e.Context.Application.Connection.WebServiceManager.AdminService.GetPermissionsByUserId(e.Context.Application.Connection.UserID);
+            List<long> mAllPermissions = new List<long>();
+            foreach (var item in mAllPermisObjects)
+            {
+                mAllPermissions.Add(item.Id);
+            }
+            if (mAllPermissions.Contains(76) && mAllPermissions.Contains(77)) //76 = Vault Set Options; 77 = Vault Get Options
+            {
+                mConfigPerm = true;
+                VaultUserCloudUploadAdminForm mAdminWindow = new VaultUserCloudUploadAdminForm();
+
+                if (Autodesk.DataManagement.Client.Framework.Forms.SkinUtils.WinFormsTheme.Instance.CurrentTheme == VDF.Forms.SkinUtils.Theme.Light)
+                {
+                    mAdminWindow.LookAndFeel.SetSkinStyle(Autodesk.iLogic.ThemeSkins.CustomThemeSkins.LightThemeName);
+                    mAdminWindow.IconOptions.Image = Properties.Resources.AdminOptionsImage_16_light;
+                }
+                if (Autodesk.DataManagement.Client.Framework.Forms.SkinUtils.WinFormsTheme.Instance.CurrentTheme == VDF.Forms.SkinUtils.Theme.Dark)
+                {
+                    //Autodesk.iLogic.ThemeSkins.CustomThemeSkins.Register();
+                    mAdminWindow.LookAndFeel.SetSkinStyle(Autodesk.iLogic.ThemeSkins.CustomThemeSkins.DarkThemeName);
+                    mAdminWindow.IconOptions.Image = Properties.Resources.AdminOptionsImage_16_dark;
+                }
+                if (Autodesk.DataManagement.Client.Framework.Forms.SkinUtils.WinFormsTheme.Instance.CurrentTheme == VDF.Forms.SkinUtils.Theme.Default)
+                {
+                    mAdminWindow.LookAndFeel.SetOffice2003Style();
+                }
+
+                mAdminWindow.ShowDialog();
+            }
+            else
+            {
+                VDF.Currency.Restriction mAdminRestriction = new VDF.Currency.Restriction("Vault User-Cloud-Upload Administration Options", "Access denied", "Accessing Administration Options requires the permissions 'Vault Get Options' and 'Vault Set Options'");
+                VDF.Forms.Settings.ShowRestrictionsSettings showRestrictionsSettings = new VDF.Forms.Settings.ShowRestrictionsSettings("Vault User-Cloud-Upload Administration Options", VDF.Forms.Settings.ShowRestrictionsSettings.IconType.Error);
+                showRestrictionsSettings.AddRestriction(mAdminRestriction);
+                showRestrictionsSettings.ShowDetailsArea = true;
+                showRestrictionsSettings.RestrictionColumnCaption = "Restriction";
+                showRestrictionsSettings.RestrictedObjectNameColumnCaption = "Object";
+                showRestrictionsSettings.ReasonColumnCaption = "Restriction Reason";
+
+                DialogResult result = VDF.Forms.Library.ShowRestrictions(showRestrictionsSettings);
+            }
+        }
+
+
+
+        public IEnumerable<CustomEntityHandler> CustomEntityHandlers()
+        {
+            return null;
+        }
+
+        public IEnumerable<DetailPaneTab> DetailTabs()
+        {
+            return null;
+        }
+
+        public IEnumerable<string> HiddenCommands()
+        {
+            return null;
+        }
+
+        public void OnLogOff(IApplication application)
+        {
+            //do nothing
+        }
+
+        public void OnLogOn(IApplication application)
+        {
+            mConnection = application.Connection;
+        }
+
+        public void OnShutdown(IApplication application)
+        {
+            //do nothing
+        }
+
+        public void OnStartup(IApplication application)
+        {
+            VDF.Library.Initialize();
+            VDF.Forms.Library.Initialize();
+        }
+
+    }
+}
---
+++ Vault-UserCloudUpload/VaultUserCloudUploadAdminForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace VaultUserCloudUpload
+{
+    partial class VaultUserCloudUploadAdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xtraTabControl1 = new DevExpress.XtraTab.XtraTabControl();
+            this.xtraTabPage1 = new DevExpress.XtraTab.XtraTabPage();
+            this.xtraTabPage2 = new DevExpress.XtraTab.XtraTabPage();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton4 = new DevExpress.XtraEditors.SimpleButton();
+            this.formAssistant1 = new DevExpress.XtraBars.FormAssistant();
+            ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).BeginInit();
+            this.xtraTabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // xtraTabControl1
+            // 
+            this.xtraTabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.xtraTabControl1.Location = new System.Drawing.Point(0, 0);
+            this.xtraTabControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.xtraTabControl1.Name = "xtraTabControl1";
+            this.xtraTabControl1.SelectedTabPage = this.xtraTabPage1;
+            this.xtraTabControl1.Size = new System.Drawing.Size(784, 481);
+            this.xtraTabControl1.TabIndex = 0;
+            this.xtraTabControl1.TabPages.AddRange(new DevExpress.XtraTab.XtraTabPage[] {
+            this.xtraTabPage1,
+            this.xtraTabPage2});
+            // 
+            // xtraTabPage1
+            // 
+            this.xtraTabPage1.Name = "xtraTabPage1";
+            this.xtraTabPage1.Size = new System.Drawing.Size(782, 456);
+            this.xtraTabPage1.Text = "File Naming";
+            // 
+            // xtraTabPage2
+            // 
+            this.xtraTabPage2.Name = "xtraTabPage2";
+            this.xtraTabPage2.Size = new System.Drawing.Size(782, 456);
+            this.xtraTabPage2.Text = "Project Properties";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.AutoSize = true;
+            this.simpleButton1.Location = new System.Drawing.Point(12, 497);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(81, 22);
+            this.simpleButton1.TabIndex = 2;
+            this.simpleButton1.Text = "Import Settings";
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.AutoSize = true;
+            this.simpleButton2.Location = new System.Drawing.Point(12, 526);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(81, 22);
+            this.simpleButton2.TabIndex = 3;
+            this.simpleButton2.Text = "Export Settings";
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.AutoSize = true;
+            this.simpleButton3.ImageOptions.Image = global::VaultUserCloudUpload.Properties.Resources.CheckOut_32_light;
+            this.simpleButton3.Location = new System.Drawing.Point(505, 509);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(120, 36);
+            this.simpleButton3.TabIndex = 4;
+            this.simpleButton3.Text = "Open from Vault";
+            // 
+            // simpleButton4
+            // 
+            this.simpleButton4.AutoSize = true;
+            this.simpleButton4.ImageOptions.Image = global::VaultUserCloudUpload.Properties.Resources.CheckIn_32_light;
+            this.simpleButton4.Location = new System.Drawing.Point(643, 509);
+            this.simpleButton4.Name = "simpleButton4";
+            this.simpleButton4.Size = new System.Drawing.Size(106, 36);
+            this.simpleButton4.TabIndex = 5;
+            this.simpleButton4.Text = "Save to Vault";
+            // 
+            // VaultUserCloudUploadAdminForm
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.White;
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.simpleButton4);
+            this.Controls.Add(this.simpleButton3);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.xtraTabControl1);
+            this.IconOptions.Image = global::VaultUserCloudUpload.Properties.Resources.AdminOptionsImage_16_light;
+            this.IsMdiContainer = true;
+            this.Name = "VaultUserCloudUploadAdminForm";
+            this.Text = "User-Cloud-Upload - Administration Options";
+            ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).EndInit();
+            this.xtraTabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraTab.XtraTabControl xtraTabControl1;
+        private DevExpress.XtraTab.XtraTabPage xtraTabPage1;
+        private DevExpress.XtraTab.XtraTabPage xtraTabPage2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton4;
+        private DevExpress.XtraBars.FormAssistant formAssistant1;
+    }
+}
